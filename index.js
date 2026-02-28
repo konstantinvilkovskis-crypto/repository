43446a82b3bc31e5e86f5b20a10434dfdbcf73b8
Konstantin <konstantin.vilkovskis@gmail.com>
2026-02-28 19:36:26 +0200

    level fixed

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let clickButton = document.querySelector("button")
 let clicks = 0
 let level = 0
+let pointsTillUpgrade = 100
 
 let clicksDisplay = document.querySelector(".points")
-let levelDisplay = document.querySelector("#a")
+let levelDisplay = document.querySelector("#level")
 
 let saveButton = document.querySelector('#saveBtn')
 let loadBtn = document.querySelector('#loadBtn')
@@ -11,10 +12,13 @@ let loadBtn = document.querySelector('#loadBtn')
 clickButton.addEventListener('click', function () {
     clicks++
     clicksDisplay.textContent = clicks
+    console.log(pointsTillUpgrade)
 
-    if (clicks == 100) {
+    if (clicks > pointsTillUpgrade) {
         level++
         levelDisplay.textContent = level
+        pointsTillUpgrade = pointsTillUpgrade + 100
+        console.log(pointsTillUpgrade)
     }
 })
 
