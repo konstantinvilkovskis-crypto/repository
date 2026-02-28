9bcf41b5284f0d6f9240fc0885f560ed95ebe0d6
Konstantin <konstantin.vilkovskis@gmail.com>
2026-02-28 13:12:59 +0200

    save and load button added

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-let a = document.querySelector("button")
-let b = 0
-let c = document.querySelector(".points")
-a.addEventListener('click',function(){
-    b++
-    c.textContent = b;
+let clickButton = document.querySelector("button")
+let clicks = 0
+let level = 0
+
+let clicksDisplay = document.querySelector(".points")
+let levelDisplay = document.querySelector("#a")
+
+let saveButton = document.querySelector('#saveBtn')
+let loadBtn = document.querySelector('#loadBtn')
+
+clickButton.addEventListener('click', function () {
+    clicks++
+    clicksDisplay.textContent = clicks
+
+    if (clicks == 100) {
+        level++
+        levelDisplay.textContent = level
+    }
 })
 
+saveButton.addEventListener('click', function () {
+    console.log("Saving clicks:", clicks);
+    localStorage.setItem("points", clicks)
+})
 
+loadBtn.addEventListener('click', function () {
+    clicks = parseInt(localStorage.getItem("points")) || 0
+    clicksDisplay.textContent = clicks
+    console.log("Loaded clicks:", clicks)
+})
+
+   
+   
 
